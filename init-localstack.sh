90fa168d920de44374f59979bd9ffdf59a15772e
Leonardo Paulo Figueiredo <leonardo.figueiredo@tecnospeed.local>
2021-08-04 15:34:45 -0300

    feat: update shell script

--- init-localstack.sh
+++ init-localstack.sh
@@ -1,7 +1,11 @@
+#!/bin/bash
+
 docker-compose up -d
 
 sleep 10
 
 aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name audio
+
+# aws --endpoint-url=http://localhost:4566 sqs send-message --queue-url http://localhost:4566/queue/audio --message-body "Mensagem de teste"
 
 # aws --endpoint-url=http://localhost:4566 sqs receive-message --queue-url http://localhost:4566/queue/audio --max-number-of-messages 10

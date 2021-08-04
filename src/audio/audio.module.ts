6a3b45e0e08a3b8cf23de6f1c596a9bd6b51e6c4
Leonardo Paulo Figueiredo <leonardo.figueiredo@tecnospeed.local>
2021-08-04 11:22:34 -0300

    feat: consume SQS queue

--- src/audio/audio.module.ts
+++ src/audio/audio.module.ts
@@ -1,19 +1,29 @@
 import { Module } from '@nestjs/common';
 import { SqsModule } from '@ssut/nestjs-sqs';
 import { AudioController } from './audio.controller';
+import { AppMessageHandler } from './audio.processor';
+
+const QUEUE_NAME = 'audio';
+const QUEUE_URL = 'http://localhost:4566/queue/audio';
 
 @Module({
   imports: [
     SqsModule.register({
-      consumers: [],
+      consumers: [
+        {
+          name: QUEUE_NAME,
+          queueUrl: QUEUE_URL,
+        },
+      ],
       producers: [
         {
-          name: 'audio',
-          queueUrl: 'http://localhost:4566/queue/audio',
+          name: QUEUE_NAME,
+          queueUrl: QUEUE_URL,
         },
       ],
     }),
   ],
   controllers: [AudioController],
+  providers: [AppMessageHandler],
 })
 export class AudioModule {}
---
+++ src/audio/audio.processor.ts
@@ -0,0 +1,19 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { SqsConsumerEventHandler, SqsMessageHandler } from '@ssut/nestjs-sqs';
+import { SQS } from 'aws-sdk';
+
+@Injectable()
+export class AppMessageHandler {
+  private readonly logger = new Logger(AppMessageHandler.name);
+
+  @SqsMessageHandler('audio')
+  public async handleMessage(message: SQS.Message) {
+    this.logger.debug(message);
+  }
+
+  @SqsConsumerEventHandler('audio', 'processing_error')
+  public onProcessingError(error: Error, message: SQS.Message) {
+    this.logger.error(error);
+    this.logger.error(message);
+  }
+}
